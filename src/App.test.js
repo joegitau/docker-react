a9fd38bcabf4e341f4c5a08e39efd6d4034a935b
Joe Karanja <joekaranja@hotmail.com>
2020-11-30 22:48:12 +0200

    add AWS deployment support config to travis.yml

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = screen.getByText(/Some Dockerized shit - alongside my boy, Travis CI!/i);
+  expect(title).toBeInTheDocument();
 });
